a4bdd2bb3087ada971f0c8896f795139fb1e66b2
czmj <clarazmjordan@gmail.com>
2025-02-28 16:30:41 +0000

    Updated from Publii

--- input/themes/slate/visual-override.js
+++
@@ -1,7 +0,0 @@
-// Custom function used to generate the output of the override css file; its content is appended to the end of the file; more about it can be found at https://getpublii.com/dev/visual-overrides/#visual-overrides
-
-var generateOverride = function (params) {
-  return '';
-}
-
-module.exports = generateOverride;
